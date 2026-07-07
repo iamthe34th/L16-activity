c56cb0bb1b58b45d6c84968910ad85eb5ecb5514
Kadhir Ravi <kadhirvelan21@gmail.com>
2026-07-07 15:08:46 +0800

    images

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const mysql = require('mysql2');
+const multer = require('multer');
 const app = express();
+
+
+
+// Set up multer for image uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -40,8 +57,17 @@ app.get('/addStudent', (req, res) => {
     res.render('addStudent');
 });
 
-app.post('/addStudent', (req, res) => {
-    const { name, dob, contact, image } = req.body;
+app.post('/addStudent', upload.single('image'), (req, res) => {
+    const { name, dob, contact } = req.body;
+
+    let image;
+
+    if (req.file) {
+        image = req.file.filename;
+    } else {
+        image = null;
+    }
+
 
     const sql = 'INSERT INTO student (name, dob, contact, image) VALUES (?, ?, ?, ?)';
 
@@ -64,18 +90,26 @@ app.get('/editStudent/:id', (req, res) => {
         if (error) {
             console.error("Error retrieving student:", error);
             res.send('Error retrieving student');
-        } else {
+        }
+
+        if (results.length > 0) {
             res.render('editStudent', {
                 student: results[0]
             });
+        } else {
+            res.send('Student not found');
         }
     });
 });
 
-app.post('/editStudent/:id', (req, res) => {
+app.post('/editStudent/:id', upload.single('image'), (req, res) => {
     const studentId = req.params.id;
-    const { name, dob, contact, image } = req.body;
+    const { name, dob, contact } = req.body;
+    let image = req.body.image;
 
+    if (req.file) {
+        image = req.file.filename;
+    }
     const sql = `
         UPDATE student
         SET name = ?, dob = ?, contact = ?, image = ?
@@ -86,13 +120,13 @@ app.post('/editStudent/:id', (req, res) => {
         [name, dob, contact, image, studentId],
         (error, results) => {
 
-        if (error) {
-            console.error("Error updating student:", error);
-            res.send('Error updating student');
-        } else {
-            res.redirect('/');
-        }
-    });
+            if (error) {
+                console.error("Error updating student:", error);
+                res.send('Error updating student');
+            } else {
+                res.redirect('/');
+            }
+        });
 });
 
 
